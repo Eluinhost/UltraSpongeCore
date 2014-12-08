3c3ca8873a71593071c90fd6647e5a6b971c9ad5
Graham Howden <graham_howden1@yahoo.co.uk>
2014-12-08 16:33:25 +0000

    feat(init): Main class with @Plugin mod annotation
    Using maven to populate the version.properties which FML looks for if version is empty or null

---
+++ src/main/java/gg/uhc/ultrahardcore/UltraHardcore.java
@@ -0,0 +1,8 @@
+package gg.uhc.ultrahardcore;
+
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "gg.uhc.ultrahardcore", name = "UltraHardcore", version = "") //version is empty, we use version.properties
+public class UltraHardcore {
+
+}
